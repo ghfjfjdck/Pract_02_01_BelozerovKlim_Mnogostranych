448349faea0e0cbb7401420a4b83ac04dcc0e67b
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2024-12-10 13:26:40 +0500

    Add files via upload

---
+++ Prakt02.01Mnogostroch web/script.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('back-btn').addEventListener('click', function () {
+    window.location.href = 'SignIn.html'; // Замените на нужный URL
+});
+
---
+++ Prakt02.01Mnogostroch web/script2.js
@@ -0,0 +1,4 @@
+
+document.getElementById('submitenter').addEventListener('click', function () {
+    window.location.href = 'Searching.html'; // Замените на нужный URL
+});
