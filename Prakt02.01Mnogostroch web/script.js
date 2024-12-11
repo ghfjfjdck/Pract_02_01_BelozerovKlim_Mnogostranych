22500e73c29d54e4ad05fc98c9a459ea90751974
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2024-12-11 16:51:39 +0500

    Delete Prakt02.01Mnogostroch web directory

--- Prakt02.01Mnogostroch web/script.js
+++
@@ -1,6 +0,0 @@
-
-
-document.getElementById('back-btn').addEventListener('click', function () {
-    window.location.href = 'SignIn.html'; // Замените на нужный URL
-});
-
--- Prakt02.01Mnogostroch web/script2.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById('submitenter').addEventListener('click', function () {
-    window.location.href = 'Searching.html'; // Замените на нужный URL
-});
